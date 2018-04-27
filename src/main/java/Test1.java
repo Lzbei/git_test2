d557403f209642b74a9cb66129038e9380423beb
lzb <lzb@zpark.com>
2018-04-27 21:24:07 +0800

    第二位用户的第一次提交

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -2,5 +2,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("aaaa");
+        System.out.println("第二位用户");
+        System.out.println("第二位用户的第一次提交");
     }
 }
